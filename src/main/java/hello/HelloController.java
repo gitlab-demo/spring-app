5b6679b6dbaf22c4de7c640eeb8e8ea9630369d2
Joel Krooswyk <jkrooswyk@gitlab.com>
2018-04-27 14:00:42 +0000

    update output message

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
         style += "body { background-color: #6DB23E; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 250%; }";
         style += "</style>";
         
-        String message = "Hello Spring Boot!";
+        String message = "Hello from Spring Boot!";
         
         String body = "<body>" + message + "</body>";
 
